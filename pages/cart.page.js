975cef7c16ce844c88e4163180a2c71407f1f3c3
unknown <fmolins97@gmail.com>
2021-12-08 19:01:32 +0100

    Added cart page

--- pages/cart.page.js
+++ pages/cart.page.js
@@ -3,8 +3,23 @@ const Page = require('./page');
 class CartPage extends Page {
     
     get productsInCart() {
-        return $('//table[@id="shopping-cart-table"]/tbody/tr');
+        return $$('//table[@id="shopping-cart-table"]/tbody/tr');
     }
+
+    async isProductInCart(productName){
+        let products = await this.productsInCart;
+        let productAddedName = '';
+        let result = false;
+        for (let product of products){
+            productAddedName = await product.$('//td[@class="product-cart-info"]/h2[@class="product-name"]/a').getText();
+            if (productAddedName.toLowerCase() == productName.toLowerCase()){
+                result = true;
+                return result;
+            }
+        }
+        return result;
+    }
+
 }
 
 module.exports = new CartPage();
